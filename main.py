b6d7a3152b0262dad835da284dba7801f27e01ef
ArthurPYth <secretbotengine@gmail.com>
2023-06-22 18:14:32 +0400

    class

---
+++ main.py
@@ -0,0 +1,16 @@
+users = [
+    {
+        "name": "Arthur"
+    },
+    {
+        "name": "Gor"
+    }
+]
+
+class Person:
+    name: None
+
+Arthur = Person
+Arthur.name = "Arthur"
+
+print(Arthur.name)
